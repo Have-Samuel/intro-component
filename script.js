d16e41ec8c0e2ff011650133a2a8eee7b29a3659
Have Samuel <samiehave9@gmail.com>
2023-03-07 23:59:04 +0300

    Validate

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const $firstInput = document.querySelector('.first-name-js');
 const $lastInput = document.querySelector('.last-name-js');
 const $emailInput = document.querySelector('.email-input-js');
 const $passwordInput = document.querySelector('.password-input-js');
+const $error = document.querySelector('.error-js');
+const $roundIcon = document.querySelector('#red-note');
+
+const heighlightError = ($input, errorMessage) => {
+  $input.classList.add('error');
+  $roundIcon.style.display = 'block';
+
+  $error.innerText = errorMessage;
+};
 
 const validate = () => {
   const firstValue = $firstInput.value.trim();
@@ -10,7 +19,15 @@ const validate = () => {
   const emailValue = $emailInput.value.trim();
   const passwordValue = $passwordInput.value.trim();
 
-  console.log('validate', firstValue, lastValue, emailValue, passwordValue);
+  if (!firstValue) {
+    heighlightError($firstInput, 'First Name cannot be empty');
+  } else if (!lastValue) {
+    heighlightError($lastInput, 'Last Name cannot be empty');
+  } else if (!emailValue) {
+    heighlightError($emailInput, 'Looks like this is not an email');
+  } else if (!passwordValue) {
+    heighlightError($passwordInput, 'Password cannot be empty');
+  }
 };
 
 $form.addEventListener('submit', (event) => {
