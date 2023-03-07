bfdeda0ad7aacf8cc7a725f00edca06e14ef12fd
Have Samuel <samiehave9@gmail.com>
2023-03-07 05:17:02 +0300

    Validating

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const $form = document.querySelector('.form-js');
-const $firstInput = document.querySelector('.first-input-js');
-const $lastInput = document.querySelector('.last-input-js');
+const $firstInput = document.querySelector('.first-name-js');
+const $lastInput = document.querySelector('.last-name-js');
 const $emailInput = document.querySelector('.email-input-js');
 const $passwordInput = document.querySelector('.password-input-js');
 
+const validate = () => {
+  const firstValue = $firstInput.value.trim();
+  const lastValue = $lastInput.value.trim();
+  const emailValue = $emailInput.value.trim();
+  const passwordValue = $passwordInput.value.trim();
 
+  console.log('validate', firstValue, lastValue, emailValue, passwordValue);
+};
 
 $form.addEventListener('submit', (event) => {
   event.preventDefault();
